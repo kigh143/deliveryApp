d3385aa2cb49280ac772e3e02338b1d963c117a2
kigh143 <ghkatende@gmail.com>
2021-03-31 19:44:27 +0300

    set up of the folder structure

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View>
+        <Header />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
